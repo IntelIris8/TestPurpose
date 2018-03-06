825d3855d40eeb2213238ea175d4e8a116c0083a
YoSoyJesseFuerte <fortejesse@yahoo.com>
2018-03-06 16:21:11 -0700

    new javascript for prompt

---
+++ app.js
@@ -0,0 +1,30 @@
+window.onload = startfunction
+
+function startfunction() {
+    //Create an input type dynamically.   
+ 
+   
+
+    var Bike = document.getElementById("Bike");
+
+
+    Dan.onclick = function() { // Note this is a function
+    alert("2 hours");
+     };
+   
+    var Penguin = document.getElementById("Penguin");
+
+
+    Alex.onclick = function() { // Note this is a function
+    alert("not reasonable");
+     };
+
+
+
+     var Car = document.getElementById("Car");
+
+
+    Bob.onclick = function() { // Note this is a function
+    alert("12 minutes");
+     };
+   }
